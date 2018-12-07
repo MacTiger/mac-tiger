fea365011cbddd841fc55134b5f701dfe959573e
Alexis Dieu <alexis.dieu@telecomnancy.net>
2018-12-07 11:04:00 +0100

    Fichier Test.java remit à la demande de Tristan

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+import org.antlr.runtime.*;
+import org.antlr.runtime.tree.*;
+
+public class Test {
+    public static void main (String [] args) throws Exception {
+        ANTLRInputStream input = new ANTLRInputStream (System.in);
+        TigerLexer lexer = new TigerLexer (input);
+        CommonTokenStream tokens = new CommonTokenStream (lexer);
+        TigerParser parser = new TigerParser (tokens);
+        parser.program ();
+        // Tree tree = (Tree) parser.program ().getTree ();
+        // System.out.println (tree.toStringTree ());
+    }
+}
