33754494a49ad79a5f8faa5130a6820001fd5013
Philippe <philippe.graff@telecomnancy.eu>
2019-03-23 16:46:54 +0100

    get registre GestionReg

---
+++ src/misc/GestionRegisters.java
@@ -0,0 +1,61 @@
+package misc;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class GestionRegisters {
+
+
+    private final int REGMAX=16;//nb max de registres R0-> R15
+    private final String PATH="misc/asm/temp.src";
+
+
+    private int regDisp;//Registres dispo : 16 -> -N
+
+
+
+    public GestionRegisters(){
+        regDisp=REGMAX;
+    }
+
+
+    //Met le registre req dans la pile
+    private void putRegisterInStack(int reg){
+        String str="\tADQ -2, SP \n \tSTW R"+String.valueOf(reg)+", (SP)\n";
+        try {
+            char buffer[]=new char[str.length()];
+            str.getChars(0,str.length(),buffer,0);
+            FileWriter fw = new FileWriter(PATH, true);
+            fw.write(buffer);
+            fw.close();
+        }
+        catch(Exception ex){
+            System.out.println("Pas le bon path");
+            ex.printStackTrace();
+        }
+    }
+
+
+
+    //Renvoie l'indice d'un registre disponible
+    public int GetRegister(){
+        if(regDisp>0){
+            return --regDisp;//Pour 16 places renvoie 15, etc.
+        }
+        else{//Registres pleins
+            int reg;
+            reg=(REGMAX-1)-(-regDisp)%REGMAX;//Registre à libérer.
+            //regDisp = 0 -> libere R15 ; regiDisp=-1 -> libère R14 etc.
+            regDisp--;
+            putRegisterInStack(reg);
+            return reg;
+        }
+    }
+
+
+    //En argument le registre ayant été libéré
+    public void freeRegister(int reg){
+
+    }
+
+}
---
+++ src/testGestionReg.java
@@ -0,0 +1,15 @@
+import misc.GestionRegisters;
+
+public class testGestionReg {
+
+
+    public static void main(String[] args){
+        GestionRegisters gr;
+        gr=new GestionRegisters();
+
+        for(int i=0 ; i<30 ; i++){
+            System.out.println(gr.GetRegister());
+        }
+    }
+
+}
